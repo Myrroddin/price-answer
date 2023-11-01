c00d400e7f8cfba9abb8107b0634ee9346961cc1
Myrroddin <psvander@gmail.com>
2023-11-01 01:59:41 -0600

    [All] Compatibility workaround for GetAddOnMetadata Blizzard API

--- Options.lua
+++ Options.lua
@@ -1,3 +1,4 @@
+local GetAddOnMetadata = C_AddOns and C_AddOns.GetAddOnMetadata or GetAddOnMetadata
 local PriceAnswer = LibStub("AceAddon-3.0"):GetAddon("PriceAnswer")
 local L = LibStub("AceLocale-3.0"):GetLocale("PriceAnswer")
 local addon_version = GetAddOnMetadata("PriceAnswer", "Version")
